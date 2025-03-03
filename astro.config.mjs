ec1ab3ca4e351b47878d814c42d001eb8550abba
Mauro de Oliveira <maurodeoliveira@MacBook-Air.local>
2025-03-03 07:52:40 -0300

    Agregado de pagina para capas

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  build: { incremental: true }, // until this is stable
+  site: 'https://maurodeoliveira.github.io',
     vite: {
         plugins: [tailwindcss()],
       },

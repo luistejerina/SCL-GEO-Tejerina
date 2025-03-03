4c2a1a8db4c21438151116fea2c44a33c8642281
Mauro de Oliveira <maurodeoliveira@MacBook-Air.local>
2025-03-03 14:56:03 -0300

    Agregado de configuracion para enrutamiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import relativeLinks from 'astro-relative-links';
-
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maurodeoliveira.github.io',
   base: 'scl-geo', // your repository name goes here, enclosed in '/'
-  integrations: [relativeLinks()],
-  output: "static",
+  output:'static',
     vite: {
         plugins: [tailwindcss()],
       },

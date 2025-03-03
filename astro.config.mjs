6099868b7423a9e3ee0c53ad6c664f0cadb0e3da
Mauro de Oliveira <maurodeoliveira@MacBook-Air.local>
2025-03-03 11:00:23 -0300

    Fix output

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import relativeLinks from 'astro-relative-links';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maurodeoliveira.github.io',
-  base: '/scl-geo', // your repository name goes here, enclosed in '/'
+  base: 'scl-geo', // your repository name goes here, enclosed in '/'
   integrations: [relativeLinks()],
   output: "static",
     vite: {

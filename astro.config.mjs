91719a849ba9e33dc14827c353f2a906d287da83
Mauro de Oliveira <maurodeoliveira@MacBook-Air.local>
2025-03-03 08:15:24 -0300

    Agregado de pagina para capas

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maurodeoliveira.github.io',
-  base: '/scl-geo/', // your repository name goes here, enclosed in '/'
+  base: '/scl-geo', // your repository name goes here, enclosed in '/'
   output: 'static',
     vite: {
         plugins: [tailwindcss()],

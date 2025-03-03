11dc97e17941cf99a95761f579d803880b93f284
Mauro de Oliveira <maurodeoliveira@MacBook-Air.local>
2025-03-03 10:03:10 -0300

    Fix output

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: 'https://maurodeoliveira.github.io',
   base: '/scl-geo', // your repository name goes here, enclosed in '/'
+  publicDir: "static",
   output: "static",
     vite: {
         plugins: [tailwindcss()],

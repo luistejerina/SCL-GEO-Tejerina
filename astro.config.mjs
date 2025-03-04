37dd88154f5af7ef96d8de7552654206858c3c44
Mauro de Oliveira <maurodeoliveira@MacBook-Air.local>
2025-03-04 20:48:55 -0300

    Mejoras en el sistema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import icon from 'astro-icon';
-
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +15,5 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
 
-  integrations: [icon()],
+  integrations: [],
 });
